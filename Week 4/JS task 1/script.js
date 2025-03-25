f7dd59c06c2607ae958f71898475827a77359922
lightning-mcqueen01 <ravikanttatiwal1999@gmail.com>
2025-03-25 14:34:27 +0530

    js tasks added

---
+++ Week 4/JS task 1/script.js
@@ -0,0 +1,18 @@
+console.log("hello");
+
+let a = 5;
+let b = 10.2;
+
+console.log(a, b);
+
+let c = "This is string";
+console.log(c);
+
+let d = 50;
+let f = 60;
+
+const sum = () => {
+  console.log(d + f);
+};
+
+sum();
