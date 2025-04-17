c64f85ad61937a2296dbca543386b012a0aae308
lightning-mcqueen01 <ravikanttatiwal1999@gmail.com>
2025-04-17 10:29:25 +0530

    jquery tasks added

--- week 5/jquery in ajax/script.js
+++ week 5/jquery in ajax/script.js
@@ -1,5 +1,16 @@
+jQuery.ajax({
+  url: "doc.txt",
+  success: function (data) {
+    alert("Document lodded successfully ");
+  },
+  error: function (xhr, error) {
+    console.log("error");
+  },
+});
+
 $(document).ready(function () {
-  $("button").click(function () {
-    $("#para").load("doc.txt");
+  $.get("doc.txt", "abhishek", function (data, status) {
+    $("#para").html(data);
+    alert("Status : " + status);
   });
 });
